e1db66f4981e6101a3165c20a16712c37618cec1
supermads <madisonsfahey@gmail.com>
2020-12-12 17:14:51 -0800

    Stage 5 of Hangman Project

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -2,12 +2,21 @@
 import random 
 
 words = ['python', 'java', 'kotlin', 'javascript']
-word_choice = random.choice(words)
-hint = word_choice[0:3]
-letters_left = len(word_choice) - 3
-print("H A N G M A N")
-guess = input("Guess the word {}: ".format(hint + ("-" * letters_left)))
-if guess == word_choice:
-    print("You survived!")
-else:
-    print("You are hanged!")
+word_choice = list(random.choice(words))
+word_set = set(word_choice)
+current = list("-" * len(word_choice))
+print("H A N G M A N\n")
+for i in range(8):
+    print(''.join(map(str, current)))
+    guess = input("Input a letter:")
+    start = 0
+    if guess in word_set:
+        for c in word_choice:
+            if c == guess:
+                index = word_choice.index(c, start)
+                current[index] = guess
+                start = index + 1
+    else:
+        print("No such letter in the word") 
+    print("\n")
+print("Thanks for playing!\nWe'll see how well you did in the next stage")
