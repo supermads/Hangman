97ba73ee7f161bac72703a242cb19ee79e6e933a
supermads <madisonsfahey@gmail.com>
2020-12-12 17:16:38 -0800

    Stage 6 of Hangman Project

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,18 +5,32 @@ words = ['python', 'java', 'kotlin', 'javascript']
 word_choice = list(random.choice(words))
 word_set = set(word_choice)
 current = list("-" * len(word_choice))
+lives = 8
+keep_playing = True
 print("H A N G M A N\n")
-for i in range(8):
-    print(''.join(map(str, current)))
-    guess = input("Input a letter:")
-    start = 0
-    if guess in word_set:
-        for c in word_choice:
-            if c == guess:
-                index = word_choice.index(c, start)
-                current[index] = guess
-                start = index + 1
-    else:
-        print("No such letter in the word") 
-    print("\n")
-print("Thanks for playing!\nWe'll see how well you did in the next stage")
+while lives > 0:
+    if len(word_set) == 0:
+        print("You guessed the word!\nYou survived!")
+        keep_playing = False
+        lives = 0
+    if keep_playing:
+        print("\n")
+        print(''.join(map(str, current)))
+        guess = input("Input a letter:")
+        start = 0
+        if guess in word_choice:
+            if guess in word_set:
+                for c in word_choice:
+                    if c == guess:
+                        index = word_choice.index(c, start)
+                        current[index] = guess
+                        start = index + 1
+                word_set.remove(guess)
+            else:
+                print("No improvements")
+                lives -= 1
+        else:
+            print("No such letter in the word")
+            lives -= 1
+if len(word_set) != 0:
+    print("You are hanged!")
