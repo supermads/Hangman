1066175f201da73367b610ab02fbeca9d7ec054e
supermads <madisonsfahey@gmail.com>
2020-12-12 17:13:39 -0800

    Stage 4 of Hangman Project

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,8 +3,10 @@ import random
 
 words = ['python', 'java', 'kotlin', 'javascript']
 word_choice = random.choice(words)
+hint = word_choice[0:3]
+letters_left = len(word_choice) - 3
 print("H A N G M A N")
-guess = input("Guess the word: ")
+guess = input("Guess the word {}: ".format(hint + ("-" * letters_left)))
 if guess == word_choice:
     print("You survived!")
 else:
